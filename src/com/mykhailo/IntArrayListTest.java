25e176dd87659075c218a9f62febce0dee3fdf24
Mykhailo <m_musienko@outlook.com>
2022-02-16 00:54:37 +0200

    Add bubble sort method

--- src/com/mykhailo/IntArrayListTest.java
+++ src/com/mykhailo/IntArrayListTest.java
@@ -12,5 +12,17 @@ class IntArrayListTest {
         System.out.println(array);
         array.remove(3, 99);
         System.out.println(array);
+        IntList array2 = new IntArrayList(5, 17, 1, 69, 0, 233, 799, 125);
+
+        for (int i = 0; i < array2.size() - 1; i++) {
+            for (int j = 0; j < array2.size() - i - 1; j++) {
+                if (array2.get(j) > array2.get(j + 1)) {
+                    int tmp = array2.get(j);
+                    array2.set(j, array2.get(j + 1));
+                    array2.set(j + 1, tmp);
+                }
+            }
+        }
+        System.out.println(array2);
     }
 }
