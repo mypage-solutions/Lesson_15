967a7812818d534be53dbfe0fc633f7f8ad512bd
Mykhailo <m_musienko@outlook.com>
2022-02-16 20:12:38 +0200

    Refactor main method

--- src/com/mykhailo/IntArrayListTest.java
+++ src/com/mykhailo/IntArrayListTest.java
@@ -4,35 +4,35 @@ import java.util.Arrays;
 
 class IntArrayListTest {
     public static void main(String[] args) {
-        int[] array1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        IntList list1 = new IntArrayList(array1);
-        System.out.println(list1);
-        list1.add(10);
-        System.out.println(list1);
-        list1.set(6, 66);
-        System.out.println(list1);
-        System.out.println("The element at index 2 is " + list1.get(2));
-        System.out.println("The length of a list is " + list1.size());
-        list1.add(6, 99);
-        System.out.println(list1);
-        list1.remove(6);
-        System.out.println(list1);
-        int[] array = list1.toArray();
+        int[] testArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        IntList testList = new IntArrayList(testArray);
+        System.out.println(testList);
+        testList.add(10);
+        System.out.println(testList);
+        testList.set(6, 66);
+        System.out.println(testList);
+        System.out.println("The element at index 2 is " + testList.get(2));
+        System.out.println("The length of a list is " + testList.size());
+        testList.add(6, 99);
+        System.out.println(testList);
+        testList.remove(6);
+        System.out.println(testList);
+        int[] array = testList.toArray();
         System.out.println(Arrays.toString(array));
 
-        int[] array2 = {5, 17, 1, 69, 0, 233, 799, 125};
-        IntList list2 = new IntArrayList(array2);
-        System.out.println("Unsorted list: " + list2);
+        int[] randomArray = {5, 17, 1, 69, 0, 233, 799, 125};
+        IntList list = new IntArrayList(randomArray);
+        System.out.println("Unsorted list: " + list);
 
-        for (int i = 0; i < list2.size() - 1; i++) {
-            for (int j = 0; j < list2.size() - i - 1; j++) {
-                if (list2.get(j) > list2.get(j + 1)) {
-                    int temp = list2.get(j);
-                    list2.set(j, list2.get(j + 1));
-                    list2.set(j + 1, temp);
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    int temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
                 }
             }
         }
-        System.out.println("Sorted list: " + list2);
+        System.out.println("Sorted list: " + list);
     }
 }
